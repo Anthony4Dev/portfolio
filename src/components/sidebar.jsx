2bb8964ac745797d0519055705a4f904d53b8246
Anthony4Dev <anthony.islan2005@gmail.com>
2023-06-13 15:24:42 -0300

    icones de redes sociais, added socialnetworks.jsx e .sass

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,15 +1,19 @@
+import SocialNetworks from './SocialNetworks';
+
 import Avatar from '../img/eu.jpg';
 
 import "../style/components/sidebar.sass";
 
+
 const sidebar = () => {
-  return (<aside id="sidebar">
-    <img src={Avatar} alt="Islan Anthony" />
-    <p className="title">Desenvolvedor Jr</p>
-    <p>Redes Sociais</p>
-    <p>Informações de Contato</p>
-    <a href="" className="btn">Download Currículo</a>
-  </aside>
+  return (
+    <aside id="sidebar">
+      <img src={Avatar} alt="Islan Anthony" />
+      <p className="title">Desenvolvedor Jr</p>
+      <SocialNetworks/>
+      <p>Informações de Contato</p>
+      <a href="" className="btn">Download Currículo</a>
+    </aside>
   );
 }
 
