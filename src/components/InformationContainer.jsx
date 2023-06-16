15e897a34057d2f121db84732851ae594d5e9e0d
Anthony4Dev <anthony.islan2005@gmail.com>
2023-06-16 19:18:25 -0300

    adicionado as cores aos icones

--- src/components/InformationContainer.jsx
+++ src/components/InformationContainer.jsx
@@ -5,7 +5,7 @@ import '../style/components/informationcontainer.sass';
 const InformationContainer = () => {
   return <section id="information">
     <div className="info-card">
-        <AiFillPhone id='phone-icon' />
+        <AiFillPhone id='phone-icon' color='#e93b81' />
         <div>
             <h3>Telefone</h3>
             <p>(81)991395701</p>
@@ -13,7 +13,7 @@ const InformationContainer = () => {
     </div>
 
     <div className="info-card">
-        <AiOutlineMail id='email-icon' />
+        <AiOutlineMail id='email-icon' color='#6ab5b9' />
         <div>
             <h3>E-mail</h3>
             <p>Anthony.islan2005@gmail.com</p>
@@ -21,7 +21,7 @@ const InformationContainer = () => {
     </div>
 
     <div className="info-card">
-        <AiFillEnvironment id='pin-icon' />
+        <AiFillEnvironment id='pin-icon' color='#c17ceb' />
         <div>
             <h3>Localização</h3>
             <p>Caruaru-PE</p>
