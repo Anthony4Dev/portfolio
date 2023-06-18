c7b7e3ebcb19d5a78f5a9df3706ad89544973259
Anthony4Dev <anthony.islan2005@gmail.com>
2023-06-18 16:13:33 -0300

    trocando as cores

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -3,9 +3,9 @@ const ProjectsContainer = () => {
     <section className='projects-container'>
       <h2>Projetos</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, numquam nihil. Voluptatem similique facilis, dicta est illum, aperiam alias, distinctio rem repudiandae harum delectus optio! Commodi eius sapiente amet ipsum?</p>
-    <a href="#" className='btn'>
-      Ver Projetos
-    </a>
+      <a href="#" className='btn'>
+        Ver Projetos
+      </a>
     </section>
   )
 }
