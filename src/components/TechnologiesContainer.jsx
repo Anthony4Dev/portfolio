5365cf7e6376dbc7f837c66c42a12ed4e4424b2c
Anthony4Dev <anthony.islan2005@gmail.com>
2023-06-17 21:17:59 -0300

    projeto base finalizado, falta apenas personaliza-lo

--- src/components/TechnologiesContainer.jsx
+++ src/components/TechnologiesContainer.jsx
@@ -1,7 +1,40 @@
+import {
+  DiJava,
+  DiCss3,
+  DiJsBadge,
+  DiNodejsSmall,
+  DiMysql,
+  DiReact,
+} from "react-icons/di";
+
+import "../style/components/technologiescontainer.sass";
+
+const technologies = [
+  { id: "java", name: "Java", icon: <DiJava/> },
+  { id: "css", name: "CSS3", icon: <DiCss3 /> },
+  { id: "js", name: "JavaScript", icon: <DiJsBadge /> },
+  { id: "node", name: "Node.js", icon: <DiNodejsSmall /> },
+  { id: "mysql", name: "MySQL", icon: <DiMysql /> },
+  { id: "react", name: "React", icon: <DiReact /> },
+];
+
 const TechnologiesContainer = () => {
   return (
-    <div>TechnologiesContainer</div>
-  )
-}
+    <section className="technologies-container">
+      <h2>Tecnologias</h2>
+      <div className="technologies-grid">
+        {technologies.map((tech) => (
+          <div className="technology-card" id={tech.id} key={tech.id}>
+            {tech.icon}
+            <div className="technology-info">
+              <h3>{tech.name}</h3>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
 
-export default TechnologiesContainer
+export default TechnologiesContainer;
