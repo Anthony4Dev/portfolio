49517b3e6c759a6bb7fafd9c0d2136c0915a027d
Anthony4Dev <anthony.islan2005@gmail.com>
2023-06-18 22:13:06 -0300

    links adicionados as redes sociais

--- src/components/SocialNetworks.jsx
+++ src/components/SocialNetworks.jsx
@@ -1,23 +1,22 @@
 import { FaLinkedinIn, FaGithub, FaInstagram } from "react-icons/fa";
-
 import "../style/components/socialnetworks.sass";
 
 const socialNetworks = [
-    { name: "linkedin", icon: <FaLinkedinIn /> },
-    { name: "github", icon: <FaGithub /> },
-    { name: "instagram", icon: <FaInstagram /> },
+  { name: "linkedin", icon: <FaLinkedinIn />, link: "https://www.linkedin.com/in/islan-anthony-649374200/" },
+  { name: "github", icon: <FaGithub />, link: "https://github.com/Anthony4Dev" },
+  { name: "instagram", icon: <FaInstagram />, link: "https://www.instagram.com/anthony_islan/" },
 ];
 
 const SocialNetworks = () => {
-    return (
+  return (
     <section id="social-networks">
-        {socialNetworks.map((network) => (
-            <a href="" className="social-btn" id={network.name} key={network.name}>
-                {network.icon}
-            </a>
-        ))}
+      {socialNetworks.map((network) => (
+        <a href={network.link} className="social-btn" id={network.name} key={network.name}>
+          {network.icon}
+        </a>
+      ))}
     </section>
-    );
+  );
 };
 
-export default SocialNetworks
+export default SocialNetworks;
