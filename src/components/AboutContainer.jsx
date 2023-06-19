e1cb40dbcabcd44d7340556757b13f1c4edf29c5
Anthony4Dev <anthony.islan2005@gmail.com>
2023-06-19 20:26:57 -0300

    colocado a descrição sobre mim e minhas tecnologias

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -2,8 +2,9 @@ const AboutContainer = () => {
     return (
         <section className="about-container">
             <h2>Sobre</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat tenetur nulla cupiditate asperiores? Tenetur, dignissimos! Ipsa officiis a cum facilis ipsam, ducimus ea quis odit consequuntur quia assumenda tenetur sapiente.</p>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus totam quidem unde tenetur soluta ipsum est, cupiditate rerum nihil, ullam obcaecati? Quibusdam sed voluptates mollitia eveniet amet minima magnam quae!</p>
+            <p>Eu sou Anthony, um apaixonado desenvolvedor web com uma sólida experiência em várias tecnologias, como HTML, CSS, JavaScript, TypeScript, PHP e Java. Minha verdadeira paixão está na criação de interfaces interativas e funcionais que proporcionam uma excelente experiência do usuário. Tenho habilidades avançadas em frameworks populares como React e React Native, permitindo-me desenvolver soluções web e mobile inovadoras.</p>
+            <p>Além disso, sou proficiente em Node.js, Git e tenho experiência em bancos de dados como MySQL e PostgreSQL. Acredito na importância da colaboração e da atenção aos detalhes para entregar projetos de alta qualidade dentro dos prazos estabelecidos. Estou sempre buscando aprimorar minhas habilidades e me manter atualizado com as últimas tendências tecnológicas.</p>
+            <p>Com minha abordagem proativa e orientada a resultados, estou preparado para enfrentar novos desafios e me adaptar a diferentes tecnologias. Estou animado para fazer parte de uma equipe dinâmica de desenvolvimento e contribuir para o sucesso de projetos empolgantes. Estou pronto para assumir novos projetos e colaborar com profissionais talentosos para criar soluções web inovadoras.</p>
         </section>
     )
 }
