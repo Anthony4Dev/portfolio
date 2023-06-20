fa483618d43e98ff855f7e7843920cc02cad27dd
Anthony4Dev <anthony.islan2005@gmail.com>
2023-06-19 21:01:15 -0300

    link para enviar para o email e destaque das tecnologias

--- src/components/InformationContainer.jsx
+++ src/components/InformationContainer.jsx
@@ -1,33 +1,43 @@
-import {AiFillPhone, AiOutlineMail, AiFillEnvironment} from 'react-icons/ai';
-
+import React from 'react';
+import { AiFillPhone, AiOutlineMail, AiFillEnvironment } from 'react-icons/ai';
 import '../style/components/informationcontainer.sass';
 
 const InformationContainer = () => {
-  return <section id="information">
-    <div className="info-card">
-        <AiFillPhone id='phone-icon' color='#e93b81' />
-        <div>
-            <h3>Telefone</h3>
-            <p>(81)991395701</p>
-        </div>
-    </div>
+  const email = 'Anthony.islan2005@gmail.com';
 
-    <div className="info-card">
-        <AiOutlineMail id='email-icon' color='#6ab5b9' />
-        <div>
-            <h3>E-mail</h3>
-            <p>Anthony.islan2005@gmail.com</p>
-        </div>
-    </div>
+  const handleEmailClick = () => {
+    window.location.href = `mailto:${email}`;
+  };
 
-    <div className="info-card">
-        <AiFillEnvironment id='pin-icon' color='#c17ceb' />
+  return (
+    <section id="information">
+      <div className="info-card">
+        <AiFillPhone id="phone-icon" color="#e93b81" />
         <div>
-            <h3>Localização</h3>
-            <p>Caruaru-PE</p>
+          <h3>Telefone</h3>
+          <p>(81)991395701</p>
         </div>
-    </div>
-  </section>
+      </div>
+
+      <div className="info-card">
+        <AiOutlineMail id="email-icon" color="#6ab5b9" />
+        <div>
+          <h3>E-mail</h3>
+          <p className="email-link" onClick={handleEmailClick}>
+            {email}
+          </p>
+        </div>
+      </div>
+
+      <div className="info-card">
+        <AiFillEnvironment id="pin-icon" color="#c17ceb" />
+        <div>
+          <h3>Localização</h3>
+          <p>Caruaru-PE</p>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default InformationContainer
+export default InformationContainer;
