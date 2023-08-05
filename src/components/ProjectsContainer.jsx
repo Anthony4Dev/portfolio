4d1ec008655ea5980394d38926e72f2a22a6e3b0
Anthony4Dev <anthony.islan2005@gmail.com>
2023-08-05 20:47:16 -0300

    colocando link e uma descrição para a lista todo

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -1,10 +1,10 @@
 const ProjectsContainer = () => {
   return (
     <section className='projects-container'>
-      <h2>Projetos</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, numquam nihil. Voluptatem similique facilis, dicta est illum, aperiam alias, distinctio rem repudiandae harum delectus optio! Commodi eius sapiente amet ipsum?</p>
-      <a href="#" className='btn'>
-        Ver Projetos
+      <h2>Lista To-Do</h2>
+      <p>Foi desenvolvida uma aplicação interativa utilizando as tecnologias React, JavaScript e CSS, com o objetivo de criar uma lista de tarefas que permite aos usuários criar, editar e marcar suas tarefas como concluídas. Durante o processo de desenvolvimento, o React foi empregado para criar componentes reutilizáveis e gerenciar o estado da aplicação, enquanto o JavaScript foi responsável pela implementação da lógica das funcionalidades da lista de tarefas. O CSS foi aplicado para garantir uma interface atraente e intuitiva. Como resultado, a lista de tarefas se tornou funcional, demonstrando efetivamente as habilidades do desenvolvedor no campo de desenvolvimento web com essas tecnologias.</p>
+      <a href="http://192.168.1.9:3000" className='btn'>
+        Ver
       </a>
     </section>
   )
